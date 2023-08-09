55617c1c82a7fc4296404fb86bdd99d7903c6a18
Qingfeng Liu <maculosa@163.com>
2023-08-09 14:07:32 +0800

    init: initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import react from '@astrojs/react'
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	site: 'https://管超.我爱你',
+	integrations: [ mdx(), sitemap()],
 });
